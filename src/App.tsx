81e4166c3167f6c2456a55117375ac7b6f8b7700
LucienPPoly <lucien.poissonnier@gmail.com>
2025-07-10 19:25:05 +0200

    working basis

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,81 @@
-import { createContext, useState } from "react";
+import { createContext, useRef, useState, type FormEvent } from "react";
 import "./App.css";
 import Button from "./components/Button";
 import InputField from "./components/InputField";
+import styled from "styled-components";
+import { CgRadioCheck } from "react-icons/cg";
+import { CgCheckO } from "react-icons/cg";
+
+const ListElement = styled.li`
+  list-style: none;
+  display: flex;
+  align-items: center;
+  gap: 15px;
+`;
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<string[]>([]);
+  const [dones, setDones] = useState<string[]>([]);
+  const [showDone, setShowDone] = useState(false);
+
+  const nameRef = useRef<HTMLInputElement>(null);
+
+  function updateShow() {
+    setShowDone(!showDone);
+  }
+
+  function addNewTask(task: string) {
+    if (nameRef.current !== null) {
+      console.log(nameRef.current.value);
+      setTodos([...todos, task]);
+    }
+  }
+
+  function taskDone(task: string) {
+    setDones([...dones, task]);
+    setTodos(todos.filter((elem) => elem !== task));
+  }
 
   return (
     <>
       <InputField
         placeholder="TODO"
-        onSumbit={() => console.log()}
+        onSumbit={() => {
+          if (nameRef.current) {
+            addNewTask(nameRef.current.value);
+            nameRef.current.value = "";
+          }
+        }}
+        inputRef={nameRef}
       ></InputField>
 
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo}>{todo}</li>
-        ))}
+      <ul className="todo">
+        {todos.map((task, index) => {
+          return (
+            <ListElement key={index} onClick={() => taskDone(task)}>
+              <CgRadioCheck className="radio"></CgRadioCheck>
+              {task}
+            </ListElement>
+          );
+        })}
       </ul>
+
+      <button className="show-done" onClick={updateShow}>
+        {showDone ? "Hide" : "Show"}
+      </button>
+
+      {showDone && (
+        <ul className="done">
+          {dones.map((task, index) => {
+            return (
+              <ListElement key={index}>
+                <CgCheckO />
+                {task}
+              </ListElement>
+            );
+          })}
+        </ul>
+      )}
     </>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { type FormEvent } from "react";
 import "./InputField.css";
 import { CgAdd } from "react-icons/cg";
 
 interface Props {
   placeholder: string;
   onSumbit: () => void;
+  inputRef: React.RefObject<HTMLInputElement>;
 }
 
-const InputField = ({ placeholder, onSumbit }: Props) => {
+const InputField = ({ placeholder, onSumbit, inputRef }: Props) => {
   return (
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSumbit();
+      }}
+    >
       <input
         type="text"
         placeholder={placeholder}
         className="input-field"
+        ref={inputRef}
       ></input>
       <CgAdd onClick={onSumbit} className="btn-add" />
     </form>
